175f7f9fdab89975bf3712d27fbe415696e05c7b
Elmaz <dremdzhe@gmail.com>
2018-08-31 12:35:30 -0700

    created books reducer and book container, react-redux containers connection explained. Redux own the state, reducer owns the data, containers have access to redux state.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return[
+        {title: 'Javacript: The Good Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dar Tower'},
+        {title: 'Eloquent Ruby'}
+    ];
+}
